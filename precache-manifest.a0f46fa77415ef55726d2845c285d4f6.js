eee48a9654ce9e50e9a3215bee9a7b6d36e98e49
SArkarHtet <developer.soearkarhtet@gmail.com>
2020-09-29 22:43:33 +0630

    Updates

---
+++ precache-manifest.a0f46fa77415ef55726d2845c285d4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4745a1fc239e9f923649a3249adc96a6",
+    "url": "/clock-app/index.html"
+  },
+  {
+    "revision": "1cc0883d4d51fd754678",
+    "url": "/clock-app/static/css/main.292fe117.chunk.css"
+  },
+  {
+    "revision": "cf6a5842bede726ec97b",
+    "url": "/clock-app/static/js/2.9833df5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock-app/static/js/2.9833df5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc0883d4d51fd754678",
+    "url": "/clock-app/static/js/main.9d0e5e9e.chunk.js"
+  },
+  {
+    "revision": "d0e9531ca03de524b875",
+    "url": "/clock-app/static/js/runtime-main.40255659.js"
+  }
+]);
